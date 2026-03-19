ed4a735038a64153aff1f85ccaa16d08b79ebacd
Raveendhar <raveendhar2007@gmail.com>
2026-03-19 08:36:04 +0530

    Add Variables.js

---
+++ JavaScript/Variables.js
@@ -0,0 +1,27 @@
+let name = "John";
+let admin = name;
+alert(admin);
+
+let n = 123;
+alert(n);
+
+let nameFieldChecked = true;
+alert(nameFieldChecked);
+
+let age = null;
+alert(age);
+
+age = 100; 
+alert(age);
+age = undefined;
+alert(age);
+
+const BIRTHDAY = "18.02.1982";
+alert(BIRTHDAY);
+
+const bigInt = 1234567890123456789012345678901234567890n;
+alert(bigInt);
+
+
+
+
